f04159e3754b3bf8a1776082f085f9d061ff85d1
danny <danny@Danica>
2024-08-28 22:18:28 +1000

    Error fix, additional validation. - Error on System Admin Accounts

--- src/main/java/hcp_pb/AdminDashboardController.java
+++ src/main/java/hcp_pb/AdminDashboardController.java
@@ -6,11 +6,14 @@ package hcp_pb;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
 /**
  * FXML Controller class
  *
@@ -18,9 +21,49 @@ import javafx.scene.control.Button;
  */
 public class AdminDashboardController implements Initializable {
 
-
     @FXML
-    private Button activateUser;
+    private Pane caseManagerPane;
+    @FXML
+    private Label headerLbl;
+    @FXML
+    private TextField searchField;
+    @FXML
+    private Button searchBtn;
+    @FXML
+    private Pane cmPane;
+    @FXML
+    private Label dashboardLbl;
+    @FXML
+    private Label manageCaseLbl;
+    @FXML
+    private Label createCaseLbl;
+    @FXML
+    private Label assessmentLbl;
+    @FXML
+    private Label carePlanLbl;
+    @FXML
+    private Label budgetPlanLbl;
+    @FXML
+    private Pane csoPane;
+    @FXML
+    private Label csoDashboardLbl;
+    @FXML
+    private Label myCaseLbl;
+    @FXML
+    private Label manageClientLbl;
+    @FXML
+    private Label newClientLbl;
+    @FXML
+    private Label reportsLbl;
+    @FXML
+    private Button addUserBtn;
+    @FXML
+    private Label headLbl;
+    @FXML
+    private Pane motherPane;
+    @FXML
+    private Pane dashboardPane;
+
     /**
      * Initializes the controller class.
      */
@@ -28,9 +71,49 @@ public class AdminDashboardController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
     @FXML
-    private void activateUser(ActionEvent event) {
+    private void dashboardLbl(MouseEvent event) {
     }
 
+    @FXML
+    private void manageCaseLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void createCaseLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void assessmentLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void carePlanLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void budgetPlanLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void csoDashboardLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void myCaseLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void manageClientLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void newClientLbl(MouseEvent event) {
+    }
+
+    @FXML
+    private void reportsLbl(MouseEvent event) {
+    }
+    
 }
--- src/main/java/hcp_pb/App.java
+++ src/main/java/hcp_pb/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
@@ -17,10 +18,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-       scene = new Scene(loadFXML("SplashScreen"), 515, 336);
-        //scene = new Scene(loadFXML("welcomeHCP"), 670, 490);
+      scene = new Scene(loadFXML("SplashScreen"), 515, 336);
+//        scene = new Scene(loadFXML("Dashboard"), 1064, 600);
+//        scene = new Scene(loadFXML("welcomeHCP"), 670, 490);
+//         scene = new Scene(loadFXML("NewClient"),520, 600);
         stage.setScene(scene);
-       
+        
+      // stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Home Care Client Profiling and Planning");
         stage.setResizable(false);
         stage.show();
